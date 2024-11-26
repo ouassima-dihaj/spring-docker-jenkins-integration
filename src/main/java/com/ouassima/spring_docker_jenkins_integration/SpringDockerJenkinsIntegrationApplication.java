2f597153fca1791dc0920a229dacdaa66d806fee
Ouassima Dihaj <ouassimadihaj1@gmail.com>
2024-11-26 22:29:38 +0100

    modify the main controller

--- src/main/java/com/ouassima/spring_docker_jenkins_integration/SpringDockerJenkinsIntegrationApplication.java
+++ src/main/java/com/ouassima/spring_docker_jenkins_integration/SpringDockerJenkinsIntegrationApplication.java
@@ -3,8 +3,10 @@ package com.ouassima.spring_docker_jenkins_integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDockerJenkinsIntegrationApplication {
 	@GetMapping("/hey")
 	public String sayHey(){
